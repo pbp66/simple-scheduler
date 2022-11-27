80f5438e3308f1098f5f8cbf573e0b3e0ce48b03
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-11-27 15:56:58 -0500

    Save and load local storage

--- assets/02-scripts/script.js
+++ assets/02-scripts/script.js
@@ -2,7 +2,29 @@
 const DateTime = luxon.DateTime;
 const now = DateTime.now();
 let currentDay = $('#currentDay');
-currentDay.text(now.toLocaleString(DateTime.DATETIME_MED));
+currentDay.text(now.toLocaleString(DateTime.DATE_HUGE));
+
+// Temporary Globals to define current working hours
+const startHour = 9;
+const endHour = 17;
+
+function loadEvents() {
+    const scheduledEvents = [];
+    const allSavedItems = Object.keys(localStorage);
+    let prefix, time;
+    for (const item of allSavedItems) {
+        [prefix, ...time] = item.split('-');
+        if (prefix === 'scheduler') {
+            scheduledEvents.push(
+                {
+                    time: time.join(' '),
+                    event: localStorage.getItem(item)
+                }
+            );
+        }
+    }
+    return scheduledEvents;
+}
 
 function createTimeBlock(hour) {
     const timeBlock = $('<p>')
@@ -12,9 +34,11 @@ function createTimeBlock(hour) {
     return timeBlock;
 }
 
-function createEventDiv(time) {  
-    const eventDiv = $('<div>').addClass('event')//.attr('id', id);
-    eventDiv.append($('<textarea>'));
+function createEventDiv(time, savedEventContent) {  
+    const eventDiv = $('<div>').addClass('event');
+    eventDiv.append(
+        $('<textarea>').val(savedEventContent)
+    );
 
     if (time.hour < now.hour) {
         eventDiv.addClass('past');
@@ -35,17 +59,33 @@ function createSaveButton() {
 }
 
 function handleOnClick(event) {
-    console.log(event);
-    console.log(event.currentTarget);
+    const scheduledEvent = 
+        event.currentTarget.parentElement.children[1].firstChild.value;
+    const scheduledTime = event.currentTarget.parentElement.firstChild.innerText;
+
+    if (scheduledEvent.length !== 0) {
+        localStorage.setItem(
+            'scheduler-' + scheduledTime.replace(' ', '-'), 
+            scheduledEvent
+        );
+
+        const alert = $('#alert');
+        const newEvent = `Saved Event to <span class="highlight">Local Storage</span>!<br>Time: ${scheduledTime}<br>Details: ${scheduledEvent}`;
+    
+        if (alert.length === 0) {
+            $('#schedule').prepend($('<p>').html(newEvent).attr('id', 'alert'));
+        } else {
+            alert.html(newEvent);
+        }
+    }
 }
 
 function init() {
-    const startHour = 9;
-    const endHour = 17;
     const dt = DateTime.fromObject({hour: startHour});
 
     const schedule = $('#schedule');
-    const list = $('<ol>');
+    const list = $('<ol>').addClass('time-block');
+    const savedEvents = loadEvents();
 
     for (let i = startHour; i <= endHour; i++) {
         const emptyListElement = $('<li>');
@@ -58,14 +98,23 @@ function init() {
             hour += time.toFormat('h') + ' AM';
         }
 
-        let rowDiv = $('<div>').addClass('row').attr('id', hour);
+        let savedEventContent = '';
+        for (let j = 0; j < savedEvents.length; j++) {
+            if (savedEvents[j].time === hour) {
+                savedEventContent = savedEvents[j].event;
+                savedEvents.splice(j, 1);
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        let rowDiv = $('<div>').addClass('row');
         rowDiv.append(
             createTimeBlock(hour),
-            createEventDiv(time),
+            createEventDiv(time, savedEventContent),
             createSaveButton()
         );
-
-
 
         emptyListElement.append(rowDiv);
         list.append(emptyListElement);
